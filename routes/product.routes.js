34fc5340c84907f97a542f7987af2ca7fa8fd350
MaaX59 <Maax.strandberg@gmail.com>
2023-07-01 12:18:13 +0200

    backend create product  missing seller

--- routes/product.routes.js
+++ routes/product.routes.js
@@ -1,8 +1,32 @@
 const express = require('express');
-const { getProducts, newProduct } = require('../controllers/productControllers');
 const router = express.Router();
+const Product = require('../models/product.model');
+const User = require('../models/User.model');
 
-router.route('/allproducts').get(getProducts);
-router.route('/newproduct').post(newProduct)
+// const { getProducts, newProduct } = require('../controllers/productControllers');
+
+// router.route('/allproducts').get(getProducts);
+// router.route('/newproduct').post(newProduct)
+
+
+router.post("/newproduct", async (req, res) => {
+   console.log(req.body)
+    try {
+        const createNewProduct = await Product.create({
+            name: req.body.name,
+            description: req.body.description,
+            price: req.body.price,
+            images: req.body.images,
+            category: req.body.category,
+            stock: req.body.stock,
+        });
+        console.log("Successful creation of new product", createNewProduct)
+        
+    } catch (error) {
+        console.log("error while reating product on the backend", error)
+        
+    }
+    
+})
 
 module.exports = router;
